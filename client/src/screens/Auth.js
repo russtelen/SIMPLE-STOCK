93adc6d0e48cece7bcfce514c25852daa0925c9e
Russell Telen <rtelen@my.bcit.ca>
2021-03-30 15:19:39 -0700

    Clean up #25

--- client/src/screens/Auth.js
+++ client/src/screens/Auth.js
@@ -1,82 +1,79 @@
-import React from 'react';
+import React from "react"
 import {
-    Container,
-    Tab,
-    Tabs,
-    TabHeading,
-    Text,
-    DefaultTabBar,
-    Content,
-} from 'native-base';
-import Login from '../components/Login';
-import Register from '../components/Register';
-import { loginUser, registerUser } from '../network';
+  Container,
+  Tab,
+  Tabs,
+  TabHeading,
+  Text,
+  DefaultTabBar,
+  Content,
+} from "native-base"
+import Login from "../components/Login"
+import Register from "../components/Register"
+import { loginUser, registerUser } from "../network"
 
 const renderTabBar = (props) => {
-    props.tabStyle = Object.create(props.tabStyle);
-    return <DefaultTabBar {...props} />;
-};
+  props.tabStyle = Object.create(props.tabStyle)
+  return <DefaultTabBar {...props} />
+}
 
 const Auth = ({ setToken }) => {
-    // Handlers
-    //-----------------------------------
-    //   Login handler
-    const submitLogin = async (data) => {
-        const token = await loginUser(data);
-        //  @TODO
-        //  Save this token to 'localStorage'
-        if (token) {
-            setToken(token.accessToken);
-        }
-        //  Then Redirect to home page
-        // console.log(token);
-    };
+  // Handlers
+  //-----------------------------------
+  //   Login handler
+  const submitLogin = async (data) => {
+    const token = await loginUser(data)
 
-    //   Register Handler
-    const submitRegister = async (data) => {
-        const token = await registerUser(data);
+    if (token.accessToken) {
+      // save token to local storage
+      setToken(token.accessToken)
+      //  Then Redirect to home page
+    }
+  }
 
-        if (token.error) {
-            alert(token.error);
-            return;
-        }
+  //   Register Handler
+  const submitRegister = async (data) => {
+    const token = await registerUser(data)
 
-        //  @TODO
-        //  Save this token to 'localStorage'
-        if (token) {
-            setToken(token.accessToken);
-        }
-        //  Then Redirect to home page
-        // console.log(token);
-    };
+    if (token.error) {
+      alert(token.error)
+      return
+    }
 
-    return (
-        <Container>
-            <Content>
-                {/* <Header hasTabs style={{ backgroundColor: "#F26F20" }} /> */}
-                <Tabs renderTabBar={renderTabBar}>
-                    <Tab
-                        heading={
-                            <TabHeading style={{ backgroundColor: '#F26F20' }}>
-                                <Text>Login</Text>
-                            </TabHeading>
-                        }
-                    >
-                        <Login submitLogin={submitLogin} />
-                    </Tab>
-                    <Tab
-                        heading={
-                            <TabHeading style={{ backgroundColor: '#F26F20' }}>
-                                <Text>Register</Text>
-                            </TabHeading>
-                        }
-                    >
-                        <Register submitRegister={submitRegister} />
-                    </Tab>
-                </Tabs>
-            </Content>
-        </Container>
-    );
-};
+    if (token.accessToken) {
+      // save token to local storage
+      setToken(token.accessToken)
+      //  Then Redirect to home page
+    }
+  }
 
-export default Auth;
+  return (
+    <Container>
+      <Content>
+        {/* <Header hasTabs style={{ backgroundColor: "#F26F20" }} /> */}
+        <Tabs renderTabBar={renderTabBar}>
+          <Tab
+            heading={
+              <TabHeading style={{ backgroundColor: "#F26F20" }}>
+                <Text>Login</Text>
+              </TabHeading>
+            }
+          >
+            <Login submitLogin={submitLogin} />
+          </Tab>
+          <Tab
+            heading={
+              <TabHeading style={{ backgroundColor: "#F26F20" }}>
+                <Text>Register</Text>
+              </TabHeading>
+            }
+          >
+            <Register submitRegister={submitRegister} />
+          </Tab>
+        </Tabs>
+      </Content>
+    </Container>
+  )
+}
+
+export default Auth
