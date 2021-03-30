9f276977080566b10ff321d83bfe424c315640f9
Russell Telen <rtelen@my.bcit.ca>
2021-03-30 10:48:18 -0700

    Fix safeareaview bug #22

--- client/src/screens/Auth.js
+++ client/src/screens/Auth.js
@@ -1,49 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 import {
-    Container,
-    Header,
-    Tab,
-    Tabs,
-    TabHeading,
-    Icon,
-    Text,
-    DefaultTabBar
-} from 'native-base';
-import Login from '../components/Login';
-import Register from '../components/Register';
-
-
+  Container,
+  Header,
+  Tab,
+  Tabs,
+  TabHeading,
+  Icon,
+  Text,
+  DefaultTabBar,
+  Content,
+} from "native-base"
+import Login from "../components/Login"
+import Register from "../components/Register"
+import { SafeAreaView, View } from "react-native"
 const renderTabBar = (props) => {
-  props.tabStyle = Object.create(props.tabStyle);
-  return <DefaultTabBar {...props} />;
-};
+  props.tabStyle = Object.create(props.tabStyle)
+  return <DefaultTabBar {...props} />
+}
 
 export default class Auth extends Component {
-    render() {
-        return (
-            <Container>
-                {/* <Header hasTabs style={{backgroundColor: '#F26F20'}}/> */}
-                <Tabs renderTabBar={renderTabBar}>
-                    <Tab 
-                        heading={
-                            <TabHeading style={{backgroundColor: '#F26F20'}} >
-                                <Text >Login</Text>
-                            </TabHeading>
-                        }
-                    >
-                        <Login />
-                    </Tab>
-                    <Tab 
-                        heading={
-                            <TabHeading style={{backgroundColor: '#F26F20'}}>
-                                <Text>Register</Text>
-                            </TabHeading>
-                        }
-                    >
-                        <Register />
-                    </Tab>
-                </Tabs>
-            </Container>
-        );
-    }
+  render() {
+    return (
+      <Container>
+        <Content>
+          {/* <Header hasTabs style={{ backgroundColor: "#F26F20" }} /> */}
+          <Tabs renderTabBar={renderTabBar}>
+            <Tab
+              heading={
+                <TabHeading style={{ backgroundColor: "#F26F20" }}>
+                  <Text>Login</Text>
+                </TabHeading>
+              }
+            >
+              <Login />
+            </Tab>
+            <Tab
+              heading={
+                <TabHeading style={{ backgroundColor: "#F26F20" }}>
+                  <Text>Register</Text>
+                </TabHeading>
+              }
+            >
+              <Register />
+            </Tab>
+          </Tabs>
+        </Content>
+      </Container>
+    )
+  }
 }
