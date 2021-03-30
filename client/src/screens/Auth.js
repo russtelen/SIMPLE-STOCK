f11f14c9490b4610848a1c1d60f95611c3d25f6f
Russell Telen <rtelen@my.bcit.ca>
2021-03-30 10:50:38 -0700

    Convert from class -> funcitonal component #22

--- client/src/screens/Auth.js
+++ client/src/screens/Auth.js
@@ -1,51 +1,49 @@
-import React, { Component } from "react"
+import React, { useState, useEffect } from "react"
 import {
   Container,
-  Header,
   Tab,
   Tabs,
   TabHeading,
-  Icon,
   Text,
   DefaultTabBar,
   Content,
 } from "native-base"
 import Login from "../components/Login"
 import Register from "../components/Register"
-import { SafeAreaView, View } from "react-native"
+
 const renderTabBar = (props) => {
   props.tabStyle = Object.create(props.tabStyle)
   return <DefaultTabBar {...props} />
 }
 
-export default class Auth extends Component {
-  render() {
-    return (
-      <Container>
-        <Content>
-          {/* <Header hasTabs style={{ backgroundColor: "#F26F20" }} /> */}
-          <Tabs renderTabBar={renderTabBar}>
-            <Tab
-              heading={
-                <TabHeading style={{ backgroundColor: "#F26F20" }}>
-                  <Text>Login</Text>
-                </TabHeading>
-              }
-            >
-              <Login />
-            </Tab>
-            <Tab
-              heading={
-                <TabHeading style={{ backgroundColor: "#F26F20" }}>
-                  <Text>Register</Text>
-                </TabHeading>
-              }
-            >
-              <Register />
-            </Tab>
-          </Tabs>
-        </Content>
-      </Container>
-    )
-  }
+const Auth = () => {
+  return (
+    <Container>
+      <Content>
+        {/* <Header hasTabs style={{ backgroundColor: "#F26F20" }} /> */}
+        <Tabs renderTabBar={renderTabBar}>
+          <Tab
+            heading={
+              <TabHeading style={{ backgroundColor: "#F26F20" }}>
+                <Text>Login</Text>
+              </TabHeading>
+            }
+          >
+            <Login />
+          </Tab>
+          <Tab
+            heading={
+              <TabHeading style={{ backgroundColor: "#F26F20" }}>
+                <Text>Register</Text>
+              </TabHeading>
+            }
+          >
+            <Register />
+          </Tab>
+        </Tabs>
+      </Content>
+    </Container>
+  )
 }
+
+export default Auth
