f681268d00e4b1e8f2dec4ddcb1ee5d8dffb0afa
karenm08 <wmok14@my.bcit.ca>
2021-04-01 01:26:45 -0700

    remove username

--- client/src/screens/Dashboard.js
+++ client/src/screens/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text, Image } from 'react-native';
-import { Container, Content } from 'native-base';
+import { Text, StyleSheet, TextInput } from 'react-native';
+import { Container, Content, List, ListItem, Body, Button } from 'native-base';
+import FooterTabs from '../components/navigation/FooterTabs';
 
 const Dashboard = () => {
     return (
@@ -13,21 +14,82 @@ const Dashboard = () => {
                         fontSize: 30,
                     }}
                 >
-                    HOME PAGE GOES HERE
+                    {/* Hi, {user.username} */}
                 </Text>
-                <Image
-                    source={{
-                        uri:
-                            'https://ahseeit.com//king-include/uploads/2021/01/131361232_212624293657437_6119095336913972420_n-5194714820.jpg',
-                    }}
-                    style={{
-                        width: 400,
-                        height: 400,
-                    }}
-                />
+                <List>
+                    <ListItem>
+                        <Body style={styles.body}>
+                            <Text style={styles.textBoxTicker}>Ticker</Text>
+                            <Text style={styles.textBox}>Price</Text>
+                            <Text style={styles.textBox}>QTY</Text>
+                            <Text style={styles.textBox}>Total</Text>
+                            <Text style={styles.textBox}>Amount</Text>
+                            <Text style={styles.textBox}>Option</Text>
+                        </Body>
+                    </ListItem>
+                    <ListItem>
+                        <Body style={styles.body}>
+                            <Text style={styles.textBox}>APL</Text>
+                            <Text style={styles.textBox}>$200</Text>
+                            <Text style={styles.textBox}>1</Text>
+                            <Text style={styles.textBox}>$400</Text>
+                            <TextInput style={styles.textInputBox}></TextInput>
+                            <Button style={styles.button}>
+                                <Text style={styles.bottonText}>Buy</Text>
+                            </Button>
+                            <Button style={styles.button}>
+                                <Text style={styles.bottonText}>Sell</Text>
+                            </Button>
+                        </Body>
+                    </ListItem>
+                    <ListItem>
+                        <Body style={styles.body}>
+                            <Text style={styles.textBox}>AAPL</Text>
+                            <Text style={styles.textBox}>$200</Text>
+                            <Text style={styles.textBox}>1</Text>
+                            <Text style={styles.textBox}>$400</Text>
+                            <TextInput style={styles.textInputBox}></TextInput>
+                            <Button style={styles.button}>
+                                <Text style={styles.bottonText}>Buy</Text>
+                            </Button>
+                            <Button style={styles.button}>
+                                <Text style={styles.bottonText}>Sell</Text>
+                            </Button>
+                        </Body>
+                    </ListItem>
+                </List>
             </Content>
         </Container>
     );
 };
+
+const styles = StyleSheet.create({
+    body: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: '1%',
+        alignItems: 'center',
+    },
+    buttonView: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+    },
+    button: {
+        backgroundColor: '#ffb347',
+        padding: '1%',
+        marginLeft: '1%',
+    },
+    bottonText: {
+        color: '#FFFF',
+        fontSize: 12,
+        textAlign: 'center',
+    },
+    textBox: {
+        width: '16%',
+        textAlign: 'center',
+    },
+    textInputBox: { borderWidth: 1, width: '15%' },
+});
 
 export default Dashboard;
