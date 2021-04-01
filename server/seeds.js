dcc095107530fdf0bef1eb0efdf33a7572cd56b9
Russell Telen <rtelen@my.bcit.ca>
2021-03-31 19:35:23 -0700

    Update  seeds.js

--- server/seeds.js
+++ server/seeds.js
@@ -6,6 +6,7 @@ dotenv.config()
 const { connectDb } = require("./models/db")
 const mongoose = require("mongoose")
 const User = require("./models/Users")
+const Transaction = require("./models/Transactions")
 
 //=============================
 // CONFIG
@@ -18,7 +19,8 @@ connectDb()
 //=============================
 
 const seedDb = async () => {
-  await User.deleteMany({});
+  await User.deleteMany({})
+  await Transaction.deleteMany({})
   console.log("seeding users")
 
   // Create new users
@@ -27,50 +29,80 @@ const seedDb = async () => {
     email: "mahboiruss@gmail.com",
     username: "russ",
     initialCash: 50000,
-    transactions:[]
+    transactions: [],
   })
   let user2 = new User({
     email: "mahsistagabi@gmail.com",
     username: "gabi",
     initialCash: 50000,
-    transactions:[]
+    transactions: [],
   })
 
+  // Save/Register Users
+  //-------------------
   await User.register(user1, "password")
+  await User.register(user2, "password")
+
+  // Create transactions
+  //-------------------
+  let transaction1 = new Transaction({
+    symbol: "AAPL",
+    numShares: 31,
+    quotePrice: -206.1,
+    transactionDateTime: 1596589501,
+  })
+
+  let transaction2 = new Transaction({
+    symbol: "TSLA",
+    numShares: 325,
+    quotePrice: -1.1,
+    transactionDateTime: 1596589520,
+  })
+
+  let transaction3 = new Transaction({
+    symbol: "TSLA",
+    numShares: 325,
+    quotePrice: 362.1,
+    transactionDateTime: 1617145438869,
+  })
+
+  // Save transactions
+  //-------------------
+  await transaction1.save()
+  await transaction2.save()
+  await transaction3.save()
+
+  // Push transactions to users
+  //-------------------
   await User.updateOne(
     { _id: user1 },
-    { $push: { transactions: 
     {
-      symbol: "AAPL",
-      numShares: 31,
-      quotePrice: -206.1,
-      transactionDateTime: 1596589501,
-    } }
-  })
-
-  await User.register(user2, "password")
-  
-  await User.updateOne(
-    { _id: user2 },
-    { $push: { transactions: 
-    
-      { $each: [{
-        symbol: "TSLA",
-        numShares: 325,
-        quotePrice: -1.1,
-        transactionDateTime: 1596589520,
+      $push: {
+        transactions: transaction1,
       },
-      {
-        symbol: "TSLA",
-        numShares: 325,
-        quotePrice: 362.1,
-        transactionDateTime: 1617145438869,
-      }]
     }
-  }}
   )
 
-  const user1Updated = await User.findOne({ 'username': 'russ' });
+  await User.updateOne(
+    { _id: user2 },
+    {
+      $push: {
+        transactions: {
+          $each: [transaction2, transaction3],
+        },
+      },
+    }
+  )
+
+  // Update Money of each user
+  //-------------------
+  user1.initialCash += transaction1.numShares * transaction1.quotePrice
+  user2.initialCash += transaction2.numShares * transaction2.quotePrice
+  user2.initialCash += transaction3.numShares * transaction3.quotePrice
+  await user1.save()
+  await user2.save()
+
+  const user1Updated = await User.findOne({ username: "russ" })
   console.log("users seeded; user1 cash: " + user1Updated.email)
 }
 
