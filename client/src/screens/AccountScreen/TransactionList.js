a1869de2e5532d00455a88a87180d0a56643a1d1
karenm08 <wmok14@my.bcit.ca>
2021-04-02 16:51:30 -0700

    convert ISO time by using moment

--- client/src/screens/AccountScreen/TransactionList.js
+++ client/src/screens/AccountScreen/TransactionList.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { List } from 'react-native-paper';
+import moment from 'moment';
 
 const TransactionList = ({ transaction }) => {
     return (
         <>
             {transaction.quotePrice > 0 ? (
                 <List.Item
-                    title={` ${transaction.symbol} `}
+                    title={`${moment(transaction.transactionDateTime).format(
+                        'YYYY-MM-DD'
+                    )}   ${transaction.symbol} `}
                     description={` SELL   No. of share : ${transaction.numShares}    Price: $${transaction.quotePrice} `}
                 />
             ) : (
                 <List.Item
-                    title={` ${transaction.symbol} `}
+                    title={`${moment(transaction.transactionDateTime).format(
+                        'YYYY-MM-DD'
+                    )}   ${transaction.symbol} `}
                     description={` BUY   No. of share : ${transaction.numShares}    Price: $${transaction.quotePrice}`}
                 />
             )}
